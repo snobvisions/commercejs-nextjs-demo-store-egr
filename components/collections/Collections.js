67dbc0d058aba3a271021bc89cee5b13f12b2a5f
snobvisions <frazier.kitron@gmail.com>
2022-05-25 02:14:16 -0500

    collections.js update

--- components/collections/Collections.js
+++ components/collections/Collections.js
@@ -1,158 +1,132 @@
 import React, { Component } from 'react';
+import Root from '../../components/common/Root';
 import Link from 'next/link';
-import Head from 'next/head';
 import { connect } from 'react-redux';
-import ProductCard from '../products/ProductCard';
-
-class Collections extends Component {
+class Confirm extends Component {
   constructor(props) {
     super(props);
-
-    this.sidebar = React.createRef();
-    this.page = React.createRef();
-
-    this.handleScroll = this.handleScroll.bind(this);
+    this.handlePrint = this.handlePrint.bind(this);
   }
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+  /**
+   * Print the window using the browser's native print functionality, if possible
+   */
+  handlePrint() {
+    if (window && window.print) {
+      window.print();
+    }
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    const animate = () => {
-      if (!this.page.current) {
-        return;
-      }
-
-      const distance =
-        this.page.current.getBoundingClientRect().bottom -
-        window.innerHeight;
-
-      if (distance < 0) {
-        this.sidebar.current.style.transform = `translateY(${distance}px)`;
-      } else {
-        this.sidebar.current.style.transform = 'translateY(0px)';
-      }
-    };
-
-    window.requestAnimationFrame(animate);
-  }
-
-  renderSidebar() {
-    const { categories } = this.props;
-
+  renderPrintButton() {
+    if (typeof window === 'undefined') {
+      return null;
+    }
     return (
-      <>
-        {categories.map(category => (
-          <div key={category.id} className="custom-container">
-            <div className="row">
-              <div className="col-2 d-none d-lg-block position-relative">
-                <p className="font-size-title font-weight-medium mb-3">
-                  {category.name}
-                </p>
-                <Link href={`/collection#${category.slug}`}>
-                  <a className="mb-5 font-color-black">
-                    <div className="d-flex">
-                      <p className="mb-2 position-relative cursor-pointer">
-                        Products
-                        <span
-                          className="position-absolute font-size-tiny text-right"
-                          style={{ right: '-12px', top: '-4px' }}
-                        >
-                          {category.products}
-                        </span>
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
-      </>
+      <button onClick={this.handlePrint} className="d-flex align-items-center text-decoration-underline cursor-pointer mt-3 mt-sm-0 no-print bg-transparent" role="button">
+        <img src="/icon/print.svg" className="mr-2 w-20 no-print"/>
+        <div className="no-print">Print Receipt</div>
+      </button>
     );
   }
 
-  /**
-   * Filter products by category
-   */
-  filterProductsByCat(catSlug) {
-    const { categories, products } = this.props;
-
-    const cat = categories.find(category => category.slug === catSlug);
-    if (!cat) {
-      return [];
-    }
-    return products.filter(product => product.categories.find(productCategory => productCategory.id === cat.id));
-  }
-
-  /**
-   * Render collections based on categories available in data
-   */
-  renderCollection() {
-    const { categories } = this.props;
-    const reg = /(<([^>]+)>)/ig;
-
-    return (
-      <div className="collection">
-        {categories.map(category => (
-          <div key={category.id}>
-              <p className="font-size-title font-weight-medium mb-4" id={category.slug}>
-                {category.name}
-              </p>
-              <div className="row mb-5 collection-1">
-                { this.filterProductsByCat(category.slug).map(product => (
-                  <div key={product.id} className="col-6 col-sm-4 col-md-3">
-                    <ProductCard
-                      permalink={product.permalink}
-                      image={product.media.source}
-                      name={product.name}
-                      price={product.price.formatted_with_symbol}
-                      description={product.description && product.description.replace(reg, '')}
-                      soldOut={product.is.sold_out}
-                    />
-                  </div>
-                ))}
-              </div>
-          </div>
-        ))}
-      </div>
-    )
-  }
-
   render() {
+    const { orderReceipt } = this.props;
     return (
-      <div className="py-5 my-5">
-        <Head>
-          <title>Collections</title>
-        </Head>
-        <div className="py-4">
-          {/* Sidebar */}
-          <div
-            ref={this.sidebar}
-            className="position-fixed left-0 right-0"
-            style={{ top: '7.5rem' }}
-          >
-            { this.renderSidebar() }
-          </div>
-
-          {/* Main Content */}
-          <div ref={this.page} className="custom-container">
-            <div className="row">
-              <div className="col-12 col-lg-10 offset-lg-2">
-                { this.renderCollection() }
+      <Root>
+        <div className="pt-5 mt-2 checkout-confirm receipt">
+          {/* Row */}
+          <div className="row mt-4">
+            <div className="col-12 col-md-10 col-lg-6 offset-md-1 offset-lg-0">
+              <div className="h-100 d-flex flex-column align-items-center justify-content-center py-5 px-4 px-sm-5">
+                <div className="bg-success700 h-64 w-64 d-flex rounded-circle align-items-center justify-content-center mb-4">
+                  <img src="/icon/check.svg" className="w-40"/>
+                </div>
+                <h3 className="text-center font-family-secondary mb-3">
+                  Thank you for your purchase!
+                </h3>
+                <h4 className="text-center font-size-subheader mb-3">
+                Your order completed successfully
+                </h4>
+                <p className="text-center font-color-light mb-5">
+                  Here is your order number for reference : {orderReceipt.customer_reference}
+                </p>
+                <div className="d-flex w-100 justify-content-center flex-column flex-sm-row">
+                  <Link href="/">
+                    <button className="checkout-confirm-buttons h-48 px-3 flex-grow-1 border bg-white border-color-gray500 font-color-light mb-2 mb-sm-0 mr-sm-2 no-print">
+                      Go back home
+                    </button>
+                  </Link>
+                  <Link href="/collection">
+                    <button className="checkout-confirm-buttons h-48 px-3 flex-grow-1 bg-black font-color-white no-print">
+                      Continue shopping
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-lg-6">
+              <div className="bg-brand300 checkout-receipt p-4 p-md-5 overflow-auto">
+                <div className="p-sm-4">
+                  <div className="border-bottom border-color-gray400 d-flex justify-content-between align-items-start pb-3 flex-column flex-sm-row">
+                    <div>
+                      <p className="font-color-light mb-2">
+                        Receipt Number: {orderReceipt.customer_reference}
+                      </p>
+                      <p className="font-size-subheader">Order Details</p>
+                    </div>
+                    { this.renderPrintButton() }
+                  </div>
+                  <div className="border-bottom border-color-gray400 d-flex align-items-start py-4 flex-column flex-sm-row">
+                    <div>
+                      <p className="font-color-light mr-4 mb-3 mb-sm-0">
+                        Ships to
+                      </p>
+                    </div>
+                    <div className="flex-grow-1">
+                      <p className="font-color-medium">{orderReceipt.shipping.street}</p>
+                      <p className="font-color-medium">{orderReceipt.shipping.town_city}, {orderReceipt.shipping.country_state}</p>
+                      <p className="font-color-medium">{orderReceipt.shipping.postal_zip_code}, {orderReceipt.shipping.country}</p>
+                    </div>
+                  </div>
+                  <div className="py-4 borderbottom border-color-gray400">
+                    {orderReceipt.order.line_items.map((item, index) => (
+                      <>
+                        <div className="d-flex flex-grow-1 mb-3">
+                          <div className="flex-grow-1">
+                            <p className="mb-2 font-weight-medium">
+                              {item.quantity} x {item.product_name}
+                            </p>
+                            <p className="font-color-light">
+                              {item.variants[0].variant_name}: {item.variants[0].option_name}
+                            </p>
+                          </div>
+                          <div className="text-right font-weight-semibold">
+                            {item.line_total.formatted_with_symbol}
+                          </div>
+                        </div>
+                        </>
+                    ))}
+                  </div>
+                  <div className="py-3 borderbottom border-color-black">
+                      <div className="d-flex justify-content-between align-items-center mb-2">
+                        <p>Subtotal</p>
+                        <p className="text-right font-weight-medium">${orderReceipt.order.total_with_tax.formatted_with_code}</p>
+                      </div>
+                  </div>
+                  <div className="d-flex justify-content-between align-items-center mb-2 pt-3">
+                    <p className="font-size-title font-weight-semibold">
+                      Order total
+                    </p>
+                    <p className="text-right font-weight-semibold font-size-title">${orderReceipt.order.total.formatted_with_code}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Root>
     );
   }
 }
 
-export default connect(state => state)(Collections);
-
+export default connect(state => state)(Confirm);
